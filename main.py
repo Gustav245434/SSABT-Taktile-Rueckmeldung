a3618f5f27705366ff5e871506cd4001aed4fb86
Paqaya <92873648+Paqaya@users.noreply.github.com>
2024-04-24 15:04:27 +0200

    main.py update in funktionalität und logik

--- main.py
+++ main.py
@@ -2,21 +2,46 @@ import machine
 import utime
 import time
 import math
-# Code zur Vibration
 
-vibrationsmotor = machine.PWM(5)
+LED = machine.Pin(5)
+led2 = machine.PWM(5)
 
-class Muster:
-    def m1(self, delay):
-        vibrationsmotor.freq(9000)
-        vibrationsmotor.duty(1000)
-        utime.sleep(delay)
-        vibrationsmotor.freq(5)
-        vibrationsmotor.duty(0)
-        utime.sleep(delay)
+
+class Motor:
+    
+    def __innit__(self, default):
+        self.__default = default
         
-muster=Muster()
+    def set_default(self, default):
+        self.__default = default
+        
+    def vibrate(value):
+        match value:
+            case 1:
+                __m1()
+            case 2:
+                __m2()
+            case 3:
+                __m3()
+                
+    def vibrate():
+        vibrate(self.__default)
+            
+    def __m1():
+        led2.freq(9000)
+        led2.duty(1000)
+        utime.sleep(1)
+        led2.freq(5)
+        led2.duty(0)
+        utime.sleep(1)
+        
+    def __m2():
+        ###
+        
+    def __m3():
+        ###
 
-muster.m1(0.5)
-muster.m1(1)
-muster.m1(0.5)
+
+m = Motor(1)
+m.vibrate()
+
